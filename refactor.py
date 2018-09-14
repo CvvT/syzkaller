948d8c664018ba7cd0ca942a9e823fe54784e709
cwt <weitengchencc@gmail.com>
2018-09-14 13:17:44 -0700

    refactor

--- refactor.py
+++ refactor.py
@@ -2,15 +2,21 @@
 
 import os
 
+def refactor(filepath):
+	content = None
+	with open(filepath, "r") as f:
+		content = f.read()
+	content = content.replace("github.com/google/syzkaller", "github.com/CvvT/syzkaller")
+	with open(filepath, "w") as f:
+		f.write(content)
+
 if __name__ == '__main__':
 	cur_path = os.path.dirname(os.path.realpath(__file__))
 	for dirName, subdirList, fileList in os.walk(cur_path):
 		for fileName in fileList:
 			if fileName.endswith(".go"):
 				real_path = os.path.join(dirName, fileName)
-				content = None
-				with open(real_path, "r") as f:
-					content = f.read()
-				content = content.replace("github.com/google/syzkaller", "github.com/CvvT/syzkaller")
-				with open(real_path, "w") as f:
-					f.write(content)
+				refactor(real_path)
+
+	real_path = os.path.join(cur_path, "Makefile")
+	refactor(real_path)
