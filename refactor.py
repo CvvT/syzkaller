24fdea5875259ae1f260f46858dace54d3826b24
cwt <weitengchencc@gmail.com>
2018-09-14 12:56:14 -0700

    check point

---
+++ refactor.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+
+import os
+
+if __name__ == '__main__':
+	cur_path = os.path.dirname(os.path.realpath(__file__))
+	for dirName, subdirList, fileList in os.walk(cur_path):
+		for fileName in fileList:
+			if fileName.endswith(".go"):
+				real_path = os.path.join(dirName, fileName)
+				content = None
+				with open(real_path, "r") as f:
+					content = f.read()
+				content.replace("github.com/google/syzkaller", "github.com/CvvT/syzkaller")
+				with open(real_path, "w") as f:
+					f.write(content)
